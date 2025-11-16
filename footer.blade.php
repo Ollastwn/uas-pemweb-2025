8c87dc95ab42c3e7ff9eb70364aa17f3bff39392
Ollastwn <ollastwngmst28@student.esaunggul.ac.id>
2025-11-16 10:07:22 +0700

    Add files via upload

---
+++ footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="mt-20 py-6 bg-white dark:bg-black border-t border-gray-200 dark:border-dark_border text-center text-gray-600 dark:text-gray">
+    <p class="text-sm">© 2025 MyApp — All rights reserved.</p>
+</footer>
